566d663fb16cd2ea8484485f29ba035634096d16
Prop-rus <63747807+Prop-rus@users.noreply.github.com>
2020-05-20 21:03:10 +0300

    Update settings.py

--- MEX3/settings.py
+++ MEX3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c^__s=$64s#7%ae05woj)c=u#j&)n4ii&1ym&690@gxt&%cqb%'
+SECRET_KEY = 'c^__s=$64s#7%ae05woj)c=u#j&)n4ii&32ds&690@gxt&%cqb%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
